343b8c4ce60e8e51da846f7e5638979b1366a57c
odedraban <oded.raban@post.runi.ac.il>
2024-11-06 19:50:49 +0200

    completed execrcises 1 2 3 4

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int a = (int) Math.ceil(Math.random() * Integer.parseInt(args[0]));
+		int b = (int) Math.ceil(Math.random() * Integer.parseInt(args[0]));
+		int c = (int) Math.ceil(Math.random() * Integer.parseInt(args[0]));
+		System.out.println(a + " " + b + " " + c);
+		int min = Math.min(a, Math.min(b, c));
+		int max = Math.max(a, Math.max(b, c));
+		int mid = (a + b + c) - (min + max);
+		System.out.println(min + " " + mid + " " + max);
+
 	}
 }
