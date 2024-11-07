21da64edf883f8831953cbea37cc35efc81a459c
odedraban <oded.raban@post.runi.ac.il>
2024-11-07 17:38:12 +0200

    completed execrcises 1 2 3 4 5

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,41 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		if ((hours < 12) && (hours != 0)){
+			System.out.print((hours) + ":");
+			if (minutes < 10){
+				System.out.println("0" + minutes + " AM");
+			}
+			else{
+				System.out.println(minutes + " AM");
+			}
+		}
+		if (hours > 12) {
+			System.out.print((hours - 12) + ":");
+			if (minutes < 10){
+				System.out.println("0" + minutes + " PM");
+			}
+			else{
+				System.out.println(minutes + " PM");
+			}
+		}
+		if (hours == 0){
+			System.out.print((hours) + ":");
+			if (minutes < 10){
+				System.out.println("0" + minutes + " AM");
+			}
+			else{
+				System.out.println(minutes + " AM");
+			}
+		}
+		if (hours == 12){
+			System.out.print((hours) + ":");
+			if (minutes < 10){
+				System.out.println("0" + minutes + " PM");
+			}
+			else{
+				System.out.println(minutes + " PM");
+			}
+		}
 	}
 }
